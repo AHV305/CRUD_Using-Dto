66786bf7718a4558b788296f18b2e52a2e7c8d49
HarshaVardhan_A <harshavardhan.angadala@gmail.com>
2025-04-13 23:04:50 +0530

    Delete ModelMapper_DTO/ModelMapper_DTO directory

--- ModelMapper_DTO/ModelMapper_DTO/src/main/java/com/ahv/crud/sb/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ahv.crud.sb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ahv.crud.sb.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
