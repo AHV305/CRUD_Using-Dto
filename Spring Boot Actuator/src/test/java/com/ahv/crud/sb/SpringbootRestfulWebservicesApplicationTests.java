fe813dff48816813503a103713ebd2c7cfe2ed88
HarshaVardhan_A <harshavardhan.angadala@gmail.com>
2025-04-15 16:21:11 +0530

    Spring Boot Actuator

---
+++ Spring Boot Actuator/src/test/java/com/ahv/crud/sb/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahv.crud.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
